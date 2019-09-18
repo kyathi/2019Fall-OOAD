90ac578e9ca83edfa3e12fd533aa6a150b5098c4
abha-123 <55116803+abha-123@users.noreply.github.com>
2019-09-18 18:11:35 -0400

    Update GuitarTest.java
    second commit

--- test/edu/fitchburgstate/csc7400/hw1/GuitarTest.java
+++ test/edu/fitchburgstate/csc7400/hw1/GuitarTest.java
@@ -1,14 +1,36 @@
 package edu.fitchburgstate.csc7400.hw1;
+/**
+ * Class : Object -Oriented Analysis and Design
+ * Professor: Orlando Montalvo
+ * Assignment HW1
+ * 
+ * student-Abha Kumari
+ * Date- 2019-09-17
+ */
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
+/**
+ * GuitarTest  will throws all exceptions in the program
+ * @author abhak
+ *
+ */
 
 public class GuitarTest {
+	/**
+	 * this will handle the exception which can be occured in the programme while running
+	 * the code
+	 * 
+	 * @throws Exception throws exceptions in the program
+	 */
 
 	@Before
 	public void setUp() throws Exception {
 	}
+	/**
+ * if this method will return error then program will fail to excecute
+ */
 
 	@Test
 	public void test() {
