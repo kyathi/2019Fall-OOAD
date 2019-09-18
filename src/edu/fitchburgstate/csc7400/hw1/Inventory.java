1cec2784d7f7750f207c82cde3d8bd49512edad7
spmsd <47991315+spmsd@users.noreply.github.com>
2019-09-18 14:28:07 -0400

    Update Inventory.java

--- src/edu/fitchburgstate/csc7400/hw1/Inventory.java
+++ src/edu/fitchburgstate/csc7400/hw1/Inventory.java
@@ -1,15 +1,38 @@
 package edu.fitchburgstate.csc7400.hw1;
+/**
+ * Class: Object-Oriented Design and Analysis
+ * Professor: Orlando Montalvo
+ * Assignment: HW 1
+ * 
+ * Date: 2018-09-18
+ */
+
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Inventory gives information about stock of the guitar available 
+ * in Rick's music store that the user searches.
+ * 
+ * @author HeadFirstOODA
+ *
+ */
 public class Inventory {
-  private List<Guitar> guitars;
-
+  private List<Guitar> guitars; //list of guitars
+  
+   /**
+   * User defined constructor 
+   * 
+   * NB: No need @params because it just initialize the list.
+   */
   public Inventory() {
-    guitars = new LinkedList<Guitar>();
+    guitars = new LinkedList<Guitar>(); 
   }
-
+  
+  /**
+   * Adds the guitar to the list.
+   */
   public void addGuitar(String serialNumber, double price,
                         String builder, String model,
                         String type, String backWood, String topWood) {
@@ -17,6 +40,10 @@ public class Inventory {
                                model, type, backWood, topWood);
     guitars.add(guitar);
   }
+  
+  /**
+   * Returns the guitar to the corresponding serialNumber.
+   */
   public Guitar getGuitar(String serialNumber) {
     for (Iterator<Guitar> i = guitars.iterator(); i.hasNext(); ) {
       Guitar guitar = (Guitar)i.next();
@@ -26,6 +53,10 @@ public class Inventory {
     }
     return null;
   }
+  
+  /**
+   * Searches and returns the matched guitar that user searches.
+   */
   public Guitar search(Guitar searchGuitar) {
     for (Iterator<Guitar> i = guitars.iterator(); i.hasNext(); ) {
       Guitar guitar = (Guitar)i.next();
