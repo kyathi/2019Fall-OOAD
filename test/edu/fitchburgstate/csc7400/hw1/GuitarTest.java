68ea519c16237466d83282e163a3145b2b1fa4b4
tamjidshaik <55214832+tamjidshaik@users.noreply.github.com>
2019-09-23 00:11:17 -0400

    Update GuitarTest.java

--- test/edu/fitchburgstate/csc7400/hw1/GuitarTest.java
+++ test/edu/fitchburgstate/csc7400/hw1/GuitarTest.java
@@ -1,3 +1,10 @@
+/**
+ * Class: Object-Oriented Design and Analysis
+ * Professor: Orlando Montalvo
+ * Assignment: HW 1
+ * Date: 2019-09-22
+ * Students:  Tamjid Shaik (@01398166)
+ */
 package edu.fitchburgstate.csc7400.hw1;
 import static org.junit.Assert.*;
 
