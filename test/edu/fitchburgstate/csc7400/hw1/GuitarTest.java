535f3d0af60dd18e2407d5f0f40765a09d36bf04
vinit <vinit@DESKTOP-A2725EL.fios-router.home>
2019-09-22 21:27:39 -0400

    added get serial number test

--- test/edu/fitchburgstate/csc7400/hw1/GuitarTest.java
+++ test/edu/fitchburgstate/csc7400/hw1/GuitarTest.java
@@ -1,3 +1,10 @@
+/* * Class: Object-Oriented Analysis and Design 
+ * * Professor: Orlando Montalvo 
+ * * Assignment: HW 1 * 
+ * Date: 2019-01-01 
+ * * Authors: Orlando Montalvo (omontalv) 
+ * Student: Vinitha Gaddampally*/
+
 package edu.fitchburgstate.csc7400.hw1;
 import static org.junit.Assert.*;
 
@@ -13,6 +20,16 @@ public class GuitarTest {
 	@Test
 	public void test() {
 		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void getSerialNumber() {
+	
+		//Testing get serial number
+		
+		Guitar guitarForVinitha = new Guitar("00001", 0, "fender", "Stratocastor", "electric", "Alder", "Alder");
+		var serialNumber = guitarForVinitha.getSerialNumber();
+		assertEquals("00001",serialNumber);
 	}
 
 }
