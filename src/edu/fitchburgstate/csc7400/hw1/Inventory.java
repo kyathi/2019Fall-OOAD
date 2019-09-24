85e029e1abafaf3be43dbf6b5de6bee1897e2264
vinit <vinit@DESKTOP-A2725EL.fios-router.home>
2019-09-24 13:34:14 -0400

    Updated inventory.java

--- src/edu/fitchburgstate/csc7400/hw1/Inventory.java
+++ src/edu/fitchburgstate/csc7400/hw1/Inventory.java
@@ -1,25 +1,51 @@
+/**
+* Class: Object-Oriented Design and Analysis
+
+* Professor: Orlando Montalvo
+
+ * Assignment: HW 1
+
+ * Student: Vinitha Gaddampally
+
+ * Date: 2019-09-20
+ */
 package edu.fitchburgstate.csc7400.hw1;
+
+// The inventory class
+
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Inventory {
-  public List<Guitar> guitars;
+/* since Inventory is a real-world entity, here it makes sense to instantiate it in the main method*/
 
+public class Inventory {
+	/**
+
+  *  Rick's guitar inventory list
+
+  */
+  List<Guitar> guitars;
+  /**
+  * Constructor
+  *
+  * Initializes guitar inventory list
+
+   */
+// constructor initializes Rick's guitar inventory list
   public Inventory() {
-	
     guitars = new LinkedList<Guitar>();
-    
   }
   
 
-  public void addGuitar(String serialNumber, double price,
+public void addGuitar(String serialNumber, double price,
                         String builder, String model,
                         String type, String backWood, String topWood) {
     Guitar guitar = new Guitar(serialNumber, price, builder,
                                model, type, backWood, topWood);
     guitars.add(guitar);
   }
+/*guitar contains the information needed to keep track of a type of guitar from Rick's music store*/
   public Guitar getGuitar(String serialNumber) {
     for (Iterator<Guitar> i = guitars.iterator(); i.hasNext(); ) {
       Guitar guitar = (Guitar)i.next();
@@ -29,7 +55,8 @@ public class Inventory {
     }
     return null;
   }
-  public Guitar search(Guitar searchGuitar) {
+  /* This method returns a list of guitars that matches with the specification of search object*/
+public Guitar search(Guitar searchGuitar) {
     for (Iterator<Guitar> i = guitars.iterator(); i.hasNext(); ) {
       Guitar guitar = (Guitar)i.next();
       // Ignore serial number since that's unique
