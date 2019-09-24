f62666afda53b47df9fa15b9c7422368c5f7f142
Komal <Komal@10.0.0.4>
2019-09-24 15:19:52 -0400

    adding javadoc comments for Guitartest class

--- test/edu/fitchburgstate/csc7400/hw1/GuitarTest.java
+++ test/edu/fitchburgstate/csc7400/hw1/GuitarTest.java
@@ -1,9 +1,22 @@
+/** Class	   : Object-Oriented Design and Analysis
+*   Professor  : Orlando Montalvo
+*   Assignment : Home Work 1
+*   Student	   : Komal(@01411726)
+*   Date	   : 2019-09-24
+*/	
+
 package edu.fitchburgstate.csc7400.hw1;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * GuitarTest class contains JUnit Test cases for Guitar class. 
+ * The purpose of Guitar test class is to test all the methods present in the Guitar class.
+ * @author Komal
+ * @version 1.0
+ */
 public class GuitarTest {
 
 	@Before
