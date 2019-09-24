f78f7e4d11f35800459e8122f831f8fed68c46b2
yamini2788 <ygopiset@student.fitchburgstate.edu>
2019-09-24 13:11:12 -0400

    Updated the Java Documentation.

--- src/edu/fitchburgstate/csc7400/hw1/Inventory.java
+++ src/edu/fitchburgstate/csc7400/hw1/Inventory.java
@@ -1,22 +1,59 @@
 package edu.fitchburgstate.csc7400.hw1;
+/**
+ * Class: Object-Oriented Design and Analysis
+ * Professor: Orlando Montalvo
+ * Assignment: HW 1
+ * Student: Yamini 
+ * Date: 2019-09-24
+ */
+
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Inventory class contains available guitars from Rick's music store
+ *@author HeadFirstOODA
+ */
 public class Inventory {
+	
+	/**
+	 * Inventory List of guitars
+	 */
   private List<Guitar> guitars;
-
-  public Inventory() {
+  
+  /**
+ 	 * Default Constructor
+ 	 * initializes the guitar list
+ 	 */
+public Inventory() {
     guitars = new LinkedList<Guitar>();
   }
 
-  public void addGuitar(String serialNumber, double price,
+/**
+ * This particular method adds guitar to the inventory
+ * 
+ * @param serialNumber 
+ * @param price store
+ * @param builder
+ * @param model
+ * @param type guitar type
+ * @param backWood
+ * @param topWood
+ */
+ public void addGuitar(String serialNumber, double price,
                         String builder, String model,
                         String type, String backWood, String topWood) {
     Guitar guitar = new Guitar(serialNumber, price, builder,
                                model, type, backWood, topWood);
     guitars.add(guitar);
   }
+ 
+ /**
+	 * Returns a guitar to the specified serial number
+	 * @param serialNumber
+	 * @return a guitar with unique serial number
+	 */
   public Guitar getGuitar(String serialNumber) {
     for (Iterator<Guitar> i = guitars.iterator(); i.hasNext(); ) {
       Guitar guitar = (Guitar)i.next();
@@ -26,6 +63,12 @@ public class Inventory {
     }
     return null;
   }
+  
+  /**
+	 * returns a guitar from the inventory based on the given guitar specification
+	 * @param searchGuitar
+	 * @return guitar
+	 */
   public Guitar search(Guitar searchGuitar) {
     for (Iterator<Guitar> i = guitars.iterator(); i.hasNext(); ) {
       Guitar guitar = (Guitar)i.next();
