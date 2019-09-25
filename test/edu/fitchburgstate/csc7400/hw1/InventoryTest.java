0d85ac0d38d0e032660628ecc89d4496a4b7d638
alolam <55292616+alolam@users.noreply.github.com>
2019-09-25 17:18:18 -0400

    Update InventoryTest.java

--- test/edu/fitchburgstate/csc7400/hw1/InventoryTest.java
+++ test/edu/fitchburgstate/csc7400/hw1/InventoryTest.java
@@ -1,6 +1,15 @@
 package edu.fitchburgstate.csc7400.hw1;
+/*
+* class: Object-Oriented Design and Analysis
+* Professor : Orlando Montavo
+* Assignment : 1
+* Student : Akhila
+* 
+* 
+*/
 
 import static org.junit.jupiter.api.Assertions.*;
+//Here we are importing all the required classes.
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,7 +20,7 @@ class InventoryTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
-		inventory = new Inventory();
+		inventory = new Inventory(); // First we are declaring inventory.
 		inventory.addGuitar("11277", 3999.95, "Collings", "CJ", "acoustic", "Indian Rosewood", "Sitka");
 		inventory.addGuitar("V95693", 1499.95, "Fender", "Stratocastor", "electric", "Alder", "Alder");
 		inventory.addGuitar("V9512", 1549.95, "Fender", "Stratocastor", "electric", "Alder", "Alder");
@@ -24,6 +33,7 @@ class InventoryTest {
 		inventory.addGuitar("566-62", 8999.95, "Ryan", "Cathedral", "acoustic", "Cocobolo", "Cedar");
 		inventory.addGuitar("6 29584", 2100.95, "PRS", "Dave Navarro Signature", "electric", "Mahogany", "Maple");
 	}
+	//we add all the guitar details to the inventory
 
 	@AfterEach
 	void tearDown() throws Exception {
@@ -32,14 +42,14 @@ class InventoryTest {
 
 	@Test
 	void testSearch() {
-		Guitar whatErinLikes = new Guitar("", 0, "fender", "Stratocastor", "electric", "Alder", "Alder");
-		Guitar guitar = inventory.search(whatErinLikes);
-		if (guitar != null) {
+		Guitar whatErinLikes = new Guitar("", 0, "fender", "Stratocastor", "electric", "Alder", "Alder"); //taking input from the user of his own choice.
+		Guitar guitar = inventory.search(whatErinLikes);//Searching for the required guitar from the inventory 
+		if (guitar != null) { //checking if the results are fetched by the inventory if details are found then display the message,model,type and price of the particular guitar
 			System.out.println("Erin, you might like this " + guitar.getManufacturer() + " " + guitar.getModel() + " "
 					+ guitar.getType() + " guitar:\n   " + guitar.getBackWood() + " back and sides,\n   "
 					+ guitar.getTopWood() + " top.\nYou can have it for only $" + guitar.getPrice() + "!");
 		} else {
-			System.out.println("Sorry, Erin, we have nothing for you.");
+			System.out.println("Sorry, Erin, we have nothing for you."); //if no details are fetched from the inventory then display the message that we have nothing for you 
 			fail("Did not find guitar");
 		}
 	}
