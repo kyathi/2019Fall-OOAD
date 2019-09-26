49183bf1ffc0df1665f5ecb32931625e43b2af10
spmsd <47991315+spmsd@users.noreply.github.com>
2019-09-25 23:28:04 -0400

    Update GuitarTest.java

--- test/edu/fitchburgstate/csc7400/hw1/GuitarTest.java
+++ test/edu/fitchburgstate/csc7400/hw1/GuitarTest.java
@@ -1,11 +1,29 @@
 package edu.fitchburgstate.csc7400.hw1;
-import static org.junit.Assert.*;
 
+/**
+ * Class: Object-Oriented Design and Analysis
+ * Professor: Orlando Montalvo
+ * Assignment: HW 1
+ * 
+ * Date: 2018-09-18
+ */
+
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * GuitarTest used to test and it throws the encountered exception
+ * 
+ * 
+ * @author HeadFirstOODA
+ *
+ */
 public class GuitarTest {
-
+	
+	/**
+   	* Handles and throws exception
+   	*/
 	@Before
 	public void setUp() throws Exception {
 	}
