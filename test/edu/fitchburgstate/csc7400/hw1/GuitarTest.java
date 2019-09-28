aa7d76af7f4115e6cf5d8336ff5078b62e7eb8b7
dmallepa <55263982+dmallepa@users.noreply.github.com>
2019-09-27 22:24:47 -0400

    Update GuitarTest.java

--- test/edu/fitchburgstate/csc7400/hw1/GuitarTest.java
+++ test/edu/fitchburgstate/csc7400/hw1/GuitarTest.java
@@ -1,19 +1,20 @@
 package edu.fitchburgstate.csc7400.hw1;
-import static org.junit.Assert.*;
-
-import org.junit.Before;
-import org.junit.Test;
-
 /*
- *Courne Name : Object Oriented Design and Analysis
+ *Course Name : Object Oriented Design and Analysis
  *Professor Name : Orlando Montalvo
  *Date : 2019-09-27
  *Assignment - HW1
  *Author : Divya Mallepally (01398602)
  */
 
-/*
- * Creating a Test testGuitar*/
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+ /*
+ * Creating a Test testGuitar
+ */
 
 public class GuitarTest {
        /**
