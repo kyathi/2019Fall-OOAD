feec1321298bbead42be7ff3b56255d363edb478
dmallepa <55263982+dmallepa@users.noreply.github.com>
2019-09-27 22:10:36 -0400

    Updated GuitarTest.java with Comments

--- test/edu/fitchburgstate/csc7400/hw1/GuitarTest.java
+++ test/edu/fitchburgstate/csc7400/hw1/GuitarTest.java
@@ -4,12 +4,28 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class GuitarTest {
+/*
+ *Courne Name : Object Oriented Design and Analysis
+ *Professor Name : Orlando Montalvo
+ *Date : 2019-09-27
+ *Assignment - HW1
+ *Author : Divya Mallepally (01398602)
+ */
 
+/*
+ * Creating a Test testGuitar*/
+
+public class GuitarTest {
+       /**
+       * Test data setup.
+       */
 	@Before
 	public void setUp() throws Exception {
 	}
-
+	
+       /**
+       * This function is to test method.
+       */
 	@Test
 	public void test() {
 		fail("Not yet implemented");
