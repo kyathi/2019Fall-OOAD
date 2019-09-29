45cb31f6f4b3e0190a7022c2b20dd5cc886ee3ea
sanasaleem669 <55260774+sanasaleem669@users.noreply.github.com>
2019-09-30 04:11:52 +0500

    Update GuitarTest.java
    my 2nd commit

--- test/edu/fitchburgstate/csc7400/hw1/GuitarTest.java
+++ test/edu/fitchburgstate/csc7400/hw1/GuitarTest.java
@@ -1,15 +1,30 @@
 package edu.fitchburgstate.csc7400.hw1;
+/**
+*class; object- oriented Analysis and Design
+*professor: Orlando Montalvo
+*Assignment HW1
+*stuedent- Sana Saleem
+*/
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
+/**
+* GuitarTest will shows all anomaly in the program
+* @author sana
+*/
 
 public class GuitarTest {
+	/**
+	* it handle the axceptions that occur in program while running the code
+	*/
 
 	@Before
 	public void setUp() throws Exception {
 	}
-
+/**
+* if method has return error then it will not execute the error
+*/
 	@Test
 	public void test() {
 		fail("Not yet implemented");
